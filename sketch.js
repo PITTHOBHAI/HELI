08d886beff0a7355753d6773d12f3159efd8bd8f
PITTHOBHAI <75744882+PITTHOBHAI@users.noreply.github.com>
2021-02-04 21:20:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var wall,wall2,wall3;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 655);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	
+	wall=createSprite(500,600,10,120);
+	wall2=createSprite(300,600,10,120);
+	wall3=createSprite(400,650,190,10);
+	
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	 ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	 World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+  packageSprite.x = packageBody.position.x; 
+  packageSprite.y = packageBody.position.y;
+
+
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false)
+
+	}
+	if(keyCode === LEFT_ARROW){
+		helicopterSprite.velocityX=-4;
+	}
+	if(keyCode === RIGHT_ARROW){
+		helicopterSprite.velocityX=4;
+	}
+}
+
+
+
